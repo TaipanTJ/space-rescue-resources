2e4293d80238ce1e004956bc6c2d4ef432da43a1
TaipanTJ <25NewtoT@mbbc.qld.edu.au>
2023-07-26 20:48:32 +1000

    rooms welcome screen edit 2

--- Rooms/WelcomeScreen.py
+++ Rooms/WelcomeScreen.py
@@ -6,3 +6,6 @@ class WelcomeScreen(Level):
     """
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
+
+        # set background image
+        self.set_background_image("Background.png")

473e847da1f6f87dfbb9e0691282bc9d0055d91b
TaipanTJ <25NewtoT@mbbc.qld.edu.au>
2023-08-22 14:08:36 +1000

    Revert "Update Asteroid.py"
    This reverts commit 1521374301223ade7e4c48488126e79b46fb9d0c.

--- Objects/Asteroid.py
+++ Objects/Asteroid.py
@@ -6,13 +6,8 @@ class Asteroid(RoomObject):
     """
     A class for Zorks danerous obstacles
     """
-<<<<<<< HEAD
 
     def __init__(speed, self, room, x, y):
-=======
-    
-    def __init__(self, room, x, y):
->>>>>>> parent of 7e1276a (asteroid speed)
         """
         Initialise the Asteroid object
         """
@@ -25,19 +20,11 @@ class Asteroid(RoomObject):
 
         # set travel direction
         angle = random.randint(135,225)
-<<<<<<< HEAD
         self.set_direction(angle, range(10-20))
         
         # register events
         self.register_collision_object("Ship")       
         self.register_collision_object("Laser")    
-=======
-        self.set_direction(angle, 10)
-
-        # register events
-        self.register_collision_object("Ship")       
-        self.register_collision_object("Laser")       
->>>>>>> parent of 7e1276a (asteroid speed)
 
     def step(self):
         """
